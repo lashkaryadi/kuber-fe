b7ef419585f9f12803cc5ab93bf942d7c6692e15
Aditya Lashkary <lashkary.adi@gmail.com>
2026-02-16 05:21:43 +0530

    VERCEL PACKAGE

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+const BASE_URL = import.meta.env.VITE_API_URL ;
 
 /* ============================
    TYPES
